aba8dbea94141a19e3d8165d88354a2dcce1bc3d
jozuah <joshua.galbas@gmail.com>
2021-05-08 00:18:38 +0200

    route register + login + logout

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let express = require('express')
 const app = express()
 var port = process.env.PORT || 3000
+
+// Librairie pour encoder mes mots de passes
+const bcrypt = require("bcryptjs")
+
+// Librairie pour les token de vérification
+const jwt = require("jsonwebtoken")
+
 require('dotenv').config()
 
 const cors = require("cors")
@@ -8,6 +15,35 @@ app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
 const url = process.env.CONNECTION_STRING
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+async function findUser(username_query) {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } ).catch(err => { console.log(err); });
+    try {
+        //On se connecte à la bonne table et on cherche si un username match avec le username en parametre
+        let result = await client.db("authentification").collection('user').findOne({ username: username_query });
+        client.close(); 
+        return result;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function createUser(username_query, password_query_hash) {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } ).catch(err => { console.log(err); });
+    try {
+        //On se connecte à la bonne table et on cherche si un username match avec le username en parametre
+        let result = await client.db("authentification").collection('user').insertOne({ username: username_query , password_hash : password_query_hash});
+        return result
+        client.close(); 
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World')
@@ -47,10 +83,168 @@ app.get('/all_meme', function (req, res) {
     })
 })
 
+//méthode post pour envoyer des données à mon serveur
+app.post('/register',async function (req, res){
+    
+    try {
+        //Définition des variables qui vont récuperer les données
+        const username_query = req.body.username;
+        const password_query = req.body.password;
+        const password_verified_query = req.body.password_verified;
+
+//////////////////     VALIDATION DES IDENTIFIANTS     //////////////////////
+
+        //Verification que chaque champ existe
+        if (!username_query || !password_query || !password_verified_query){
+            return res.status(400).json({errorMessage : "Please enter all required field"})
+        }
+
+        //Verification que le mot de passe est assez long
+        if(password_query.length < 5){
+            return res.status(400).json({errorMessage : "Password is too short"})
+        }
+
+        //Verification que les deux mots de passes coïncident
+        if(password_query !== password_verified_query){
+            return res.status(400).json({errorMessage : "Please enter same password"})
+        }
+
+        //Verification que le user n'existe pas
+        let existingUser =  await findUser(username_query);
+        //console.log('mon user :', existingUser)
+        if (existingUser != null){
+            return res.status(400).json({errorMessage : "Existing user"})
+        }
+
+///////////////////////////    HASH THE PASSWORD    ////////////////////////////////////
+
+        const salt = await bcrypt.genSalt();
+        const password_query_hash = await bcrypt.hash(password_query,salt);
+        
+        //Voir le mot de passe encrypté
+        //console.log(password_query_hash)
 
 
+//////////////////////////     CREATE A NEW USER     ////////////////////////////////////
+        let newUser = await createUser(username_query, password_query_hash);
+
+
+/////////////////////////   LOG USER 1      //////////////////////////////
+
+        //Je crée un token avec l'id venant de la db et le password de mon serveur stocké dans le .env
+        const token = jwt.sign(
+            {
+                user: newUser._id,
+            },
+            process.env.JWT_PASSWORD
+        ); 
+        
+        //console.log(token);
+
+        //stockage du token dans les cookies, la variable token aura le nom token dans les cookies, et la route renvoie la string positive
+        res.cookie("token", token, {
+            httpOnly : true,
+        }).send({
+                'response : ' : "A new user has been created",
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).send();
+    }
+
+
+})
+
+
+app.post('/login', async function (req, res){
+    try {
+        //Définition des variables qui vont récuperer les données
+        const username_query = req.body.username;
+        const password_query = req.body.password;
+
+        //Verification que chaque champ existe => seulement 2 champs
+        if (!username_query || !password_query ){
+            return res.status(400).json({errorMessage : "Please enter all required field"})
+        }
+
+        //On va chercher si le User existe donc on réutilise la fonction de register qui cherche une utilisateur
+        const matchUser = await findUser(username_query);
+        if (!matchUser) {
+            // 400 - Bad Request | 401 - Unauthorized Request
+            return res.status(401).json({errorMessage : "Wrong username or password"})
+        }
+
+        //On compare le mot de passe de la requete avec celui stocké dans la BDD => password_hash. Retourne "true" si y a match
+        const matchPassword = await bcrypt.compare(password_query, matchUser.password_hash);
+
+        if (matchPassword == false){
+            return res.status(401).json({errorMessage : "Wrong username or password"})
+        }
+
+        //Je crée un token avec l'id venant de la db et le password de mon serveur stocké dans le .env
+        const token = jwt.sign(
+            {
+                user: matchUser._id,
+            },
+            process.env.JWT_PASSWORD
+        ); 
+
+        //stockage du token dans les cookies, la variable token aura le nom token dans les cookies, et la route renvoie la string positive
+        res.cookie("token", token, {
+            //seul mon browser peu lire le token et pas le js
+            httpOnly : true,
+        }).send({
+            'response : ' : "Logged in !! Bien joué",
+        })
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send();
+    }
+})
+
+//vider les cookie => log out
+app.get("/logout", function(req, res) {
+    //je remplace mon token par une string vie et je le détruit => double destruction
+    res.cookie("token","", {
+        //seul mon browser peu lire le token et pas le js
+        httpOnly : true,
+        //je donne une date qui est déja passé donc le cookie va disparaitre
+        expires : new Date(0)
+    }).send({
+        'response : ' : 'You have been logged out'
+    })
+})
+
+//Endpoint privé (seul un utilisateur authentifié peut l'utiliser) qui met un lien dans la BDD avec le username
+// on va tester dans postman avec un token existant : "token : ": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjA5NTZkMDY1OWY3NTk0NmJkNjA4YTBhIiwiaWF0IjoxNjIwNDA4NDU1fQ.Is98mbNhapVixYF2uO2UHFPmyph7ZL2esfMYH6KqTww"
+app.post('/add_meme', async function (req, res){
+    try {
+        const name_meme_query = req.body.name_meme
+        const token_query = req.body.token
+        
+        // Le original token sera celui stocké dans les cookies ou 
+        const token_storage = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjA5NTZkMDY1OWY3NTk0NmJkNjA4YTBhIiwiaWF0IjoxNjIwNDA4NDU1fQ.Is98mbNhapVixYF2uO2UHFPmyph7ZL2esfMYH6KqTww"
+
+        
+        jwt.verify(token_query, token_storage, function (err, decoded){
+            if(err){
+              res.send({
+                  'response : ' : "Vous pouvez enregister un fichier"
+              }
+              );
+            } else {
+              res.send("fail token");
+            }
+        });        
+
+    } catch(err) {
+        console.error(err);
+        res.status(500).send();
+    } 
+})
 
 app.listen(port, function () {
-  console.log('Hello world app listening on port ' + port )
+  console.log('Server on port ' + port )
 })
 
